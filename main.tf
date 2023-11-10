ef3351559025bcb08545ad0882f15dcd4232bc00
VIJAYAKUMARAN <106998045+VIJAYAKUMARAN03@users.noreply.github.com>
2023-11-10 14:23:41 +0530

    new commit
    1st commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIARRQWCHXFET2DL4MS"
+  secret_key = "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"
+}
+
+resource "aws_instance" "my_first_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+}
+
+# resource "aws_instance" "my_first_instance" {
+#     ami = "ami-0fc5d935ebf8bc3bc"
+#     instance_type = "t2.micro"
+# }
+
+# resource "aws_vpc" "first_vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "First_VPC"
+#   }
+# }
+
+# resource "aws_subnet" "subnet-1"{
+#   vpc_id = "vpc-0593658918ee930b1"
+#   cidr_block = "172.31.0.0/20"
+#   tags = {
+#     Name = "First_subnet"
+#   }
+# } 
---
+++ var.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+  default = "ami-0fc5d935ebf8bc3bc"
+
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
