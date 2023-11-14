bf7b3019c1fa78a2bcadff2d5b8786b4d19cfc62
VIJAYAKUMARAN03 <vijayakumaran27062003>
2023-11-14 21:12:50 +0530

    main updated

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
-resource "aws_instance" "my_first_instance" {
+resource "aws_instance" "first_instance" {
     ami = "${var.ami}"
-    instance_type = var.instance_type
+    instance_type = "${var.instance_type}"
     tags = {
-      Name = "vijay_instance_"
+      "Name" = "vijay_instance_"
     }
 }
 
