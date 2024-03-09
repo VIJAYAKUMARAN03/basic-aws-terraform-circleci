2f4cf58ad563a94487227c757a219af9f4a6ec25
VIJAYAKUMARAN <106998045+VIJAYAKUMARAN03@users.noreply.github.com>
2024-03-09 17:13:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
   region = "${var.region}"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-rp-states" //your tfstate bucket name 
+    key    = "dev.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 resource "aws_instance" "first_instance" {
